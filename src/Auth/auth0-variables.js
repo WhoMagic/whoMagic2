489ad579d8bd880c3bbb554ba9d41f165542d78f
Eliot <mr.ear84@gmail.com>
2018-06-18 09:26:57 -0400

    adding Auth0 integration needs linked in

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,11 @@
+var {domain} = require('./keys');
+var {clientId} = require('./keys');
+var {callBackURL} = require('./keys');
+
+
+export const AUTH_CONFIG = {
+  domain: domain,
+  clientId: clientId,
+  callbackUrl: callBackURL
+}
+
